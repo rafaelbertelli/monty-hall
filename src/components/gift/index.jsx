85153501af7110ff87e11b9555c441ed1a737849
Rafael Borges <rafael.borges@luizalabs.com>
2021-08-11 20:50:52 -0300

    structuring the project

---
+++ src/components/gift/index.jsx
@@ -0,0 +1,12 @@
+import styles from "./Gift.module.css";
+
+export default function Gift() {
+  return (
+    <div className={styles.gift}>
+      <div className={styles.top}></div>
+      <div className={styles.body}></div>
+      <div className={styles.bowbox1}></div>
+      <div className={styles.bowbox2}></div>
+    </div>
+  );
+}
