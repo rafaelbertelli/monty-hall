d5d69d1b468efe2dbe93bc4e8d3dd8a39ddd9a6f
Rafael Borges <rafael.borges@luizalabs.com>
2021-08-18 17:51:23 -0300

    transform js to ts file

--- src/components/door/index.tsx
+++ src/components/door/index.tsx
@@ -1,13 +1,22 @@
+import DoorModel from "../../model/door";
 import styles from "./Door.module.css";
 
-export default function Door(props) {
-  const selectedDoor = props.selected ? styles.selectedDoor : "";
+interface DoorProps {
+  door: DoorModel;
+}
+
+export default function Door(props: DoorProps) {
+  const {
+    door: { number, selected },
+  } = props;
+
+  const selectedDoor = selected ? styles.selectedDoor : "";
 
   return (
     <div className={styles.area}>
       <div className={`${styles.frame} ${selectedDoor}`}>
         <div className={styles.door}>
-          <div className={styles.number}>9</div>
+          <div className={styles.number}>{number}</div>
           <div className={styles.knob}></div>
         </div>
       </div>
