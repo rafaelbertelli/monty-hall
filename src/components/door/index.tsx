6134604a3638100c3961d4500a49c094482c8f4e
Rafael Borges <rafael.borges@luizalabs.com>
2021-08-19 21:23:50 -0300

    handle with opened door

--- src/components/door/index.tsx
+++ src/components/door/index.tsx
@@ -9,17 +9,24 @@ interface DoorProps {
 export default function Door(props: DoorProps) {
   const { value, onChange } = props;
 
-  const selectedDoor = value.selected ? styles.selectedDoor : "";
+  const selectedDoor =
+    value.selected && !value.opened ? styles.selectedDoor : "";
 
   const toggleSelection = (e) => onChange(value.toggleSelection());
+  const openDoor = (e) => {
+    e.stopPropagation();
+    onChange(value.open());
+  };
 
   return (
     <div className={styles.area} onClick={toggleSelection}>
       <div className={`${styles.frame} ${selectedDoor}`}>
-        <div className={styles.door}>
-          <div className={styles.number}>{value.number}</div>
-          <div className={styles.knob}></div>
-        </div>
+        {!value.opened && (
+          <div className={styles.door}>
+            <div className={styles.number}>{value.number}</div>
+            <div className={styles.knob} onClick={openDoor}></div>
+          </div>
+        )}
       </div>
       <div className={styles.floor}></div>
     </div>
